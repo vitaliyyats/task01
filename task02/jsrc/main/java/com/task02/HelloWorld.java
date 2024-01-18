c3281d3f95ebefb02d3f6734a2a29cff2a6ff578
Vitalii_Yatsunskyi <vitalii_yatsunskyi@epam.com>
2024-01-18 12:33:09 +0200

    task02 fix

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -10,8 +10,7 @@ import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
         roleName = "hello_world-role",
-        isPublishVersion = true,
-        aliasName = "${lambdas_alias_name}"
+        isPublishVersion = true
 )
 @LambdaUrlConfig(authType = AuthType.NONE,
         invokeMode = InvokeMode.BUFFERED
